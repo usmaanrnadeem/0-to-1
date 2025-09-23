4900067f78f5ef4b7fc33071684963a3b6dbc14b
usmaanrnadeem <usmaanrnadeem@outlook.com>
2025-09-23 22:57:27 +0100

    Complete 07-array-cardio-day-2 exercise

--- the-odin-project-foundations/js30-exercises/07-array-cardio-day-2.js
+++ the-odin-project-foundations/js30-exercises/07-array-cardio-day-2.js
@@ -17,12 +17,23 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+
+let nineteenOrOlderSome = people.some(person => (2025 - person.year) >= 19)
+
 // Array.prototype.every() // is everyone 19 or older?
+
+let nineteenOrOlderEvery = people.every(person => (2025 - person.year) >= 19)
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
 
+let findId = comments.find(comment => comment.id == 823423)
+
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+
+comments.splice(comments.findIndex(comment => comment.id == 823423),1)
+
+
