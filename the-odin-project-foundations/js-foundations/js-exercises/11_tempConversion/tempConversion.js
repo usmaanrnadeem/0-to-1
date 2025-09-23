44da7388a8c761b9432a7c416826b662a983cf84
usmaanrnadeem <usmaanrnadeem@outlook.com>
2025-09-23 23:43:36 +0100

    Complete 11_tempConversion exercise

--- the-odin-project-foundations/js-foundations/js-exercises/11_tempConversion/tempConversion.js
+++ the-odin-project-foundations/js-foundations/js-exercises/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  return Math.round((F-32)*(5/9)*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  return Math.round((C/(5/9)+32)*10)/10
 };
 
 // Do not edit below this line
