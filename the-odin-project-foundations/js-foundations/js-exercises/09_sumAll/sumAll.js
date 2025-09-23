d6a5df9e0f7c55b8d8940c109574d682ef9bd2a9
usmaanrnadeem <usmaanrnadeem@outlook.com>
2025-09-23 23:33:26 +0100

    Complete 09_sumAll exercise

--- the-odin-project-foundations/js-foundations/js-exercises/09_sumAll/sumAll.js
+++ the-odin-project-foundations/js-foundations/js-exercises/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let min, max;
+    if (a < 0 || b < 0 || a%1 !== 0 || b%1 !== 0 || typeof(a) !== "number" || typeof(b) !== "number") {
+        return "ERROR"
+    }
+    if (a <= b) {
+        min = a;
+        max = b;
+    } 
+    else {
+        min = b;
+        max = a;
+    }
 
+    let sum = 0;
+
+    for (let i = min; i <= max; i++) {
+        sum += i
+    }
+
+    return sum
 };
 
 // Do not edit below this line
