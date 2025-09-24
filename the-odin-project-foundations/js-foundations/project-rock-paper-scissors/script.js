95c470420a82c5be1717f8f7bbade64fdec6d5ee
usmaanrnadeem <usmaanrnadeem@outlook.com>
2025-09-24 19:01:06 +0100

    Add GUI to project:rock-paper-scissors

--- the-odin-project-foundations/js-foundations/project-rock-paper-scissors/script.js
+++ the-odin-project-foundations/js-foundations/project-rock-paper-scissors/script.js
@@ -11,43 +11,111 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let x = prompt("Please enter your move: ")
-    return x.toLowerCase()
-}
+function playRound (computerChoice, humanChoice) {
 
-function playGame () {
-
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound (computerChoice, humanChoice) {
-        if (computerChoice == humanChoice) {
-            return "This round was a draw!"
-        }
-        else if (computerChoice == "rock" && humanChoice == "scissors" || computerChoice == "paper" && humanChoice == "rock" || computerChoice == "scissors" && humanChoice == "paper") {
-            ++computerScore;
-            return `You lose! ${computerChoice} beats ${humanChoice}.}`
-        }
-        else {
-            ++humanScore;
-            return `You win! ${humanChoice} beats ${computerChoice}.`
-        }
+    if (playerMoveBox.hasChildNodes || compMoveBox.hasChildNodes) {
+        playerMoveBox.replaceChildren()
+        compMoveBox.replaceChildren()
     }
 
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound(getComputerChoice(),getHumanChoice()));
-    }
+    let roundResult;
+    
+    const playerMove = document.createElement("p");
+    playerMove.textContent = humanChoice;
 
-    if (humanScore > computerScore) {
-        return `Congratulations! You won with a score of ${humanScore} - ${computerScore}`
+    const computerMove = document.createElement("p");
+    computerMove.textContent = computerChoice;
+
+    playerMoveBox.appendChild(playerMove);
+    compMoveBox.appendChild(computerMove);
+
+    if (computerChoice == humanChoice) {
+        roundResult = "This round was a draw!"
     }
-    else if (humanScore < computerScore) {
-        return `Unlucky! You lost with a score of ${computerScore} - ${humanScore}`
+    else if (computerChoice == "rock" && humanChoice == "scissors" || computerChoice == "paper" && humanChoice == "rock" || computerChoice == "scissors" && humanChoice == "paper") {
+        ++computerScore;
+        computerScoreCounter.textContent = `Computer's Score: ${computerScore}`;
+        roundResult = `You lose! ${computerChoice} beats ${humanChoice}.}`
     }
     else {
-        return `Draw! The final score was ${computerScore} - ${humanScore}`
+        ++humanScore;
+        humanScoreCounter.textContent = `Your Score: ${humanScore}`
+        roundResult = `You win! ${humanChoice} beats ${computerChoice}.`
+    }
+
+    if (computerScore == 5 || humanScore == 5) {
+        const winner = document.createElement("h1");
+        if (computerScore == 5) {
+            winner.textContent = "Computer Wins!"
+        }  else {
+            winner.textContent = "You Win!"
+        }
+        results.appendChild(winner)
+
+    return roundResult
+
     }
 }
 
-console.log(playGame())
+
+let humanScore = 0;
+let computerScore = 0;
+
+const selection = document.querySelector(".selection");
+const playerMoveBox = document.querySelector(".selection .player");
+const compMoveBox = document.querySelector(".selection .comp")
+
+console.log(playerMoveBox)
+
+const rock = document.querySelector("button.rock");
+const paper = document.querySelector("button.paper");
+const scissors = document.querySelector("button.scissors");
+
+const humanScoreCounter = document.querySelector(".hScore");
+const computerScoreCounter = document.querySelector(".cScore");
+
+const results = document.querySelector(".results");
+
+const reset = document.querySelector(".reset button")
+
+if (humanScore == 0) {
+    humanScoreCounter.textContent = "Your Score: 0"
+}
+
+if (computerScore == 0) {
+    computerScoreCounter.textContent = "Computer's Score: 0"
+}
+
+rock.addEventListener("click", event => {
+    if (computerScore < 5 && humanScore < 5) {
+        playRound(getComputerChoice(),event.target.textContent)
+    }
+});
+
+paper.addEventListener("click", event => {
+    if (computerScore < 5 && humanScore < 5) {
+        playRound(getComputerChoice(),event.target.textContent)
+    }
+});
+scissors.addEventListener("click", event => {
+    if (computerScore < 5 && humanScore < 5) {
+        playRound(getComputerChoice(),event.target.textContent)
+    }
+});
+
+reset.addEventListener("click", event => {
+    humanScore = 0;
+    computerScore = 0;
+
+    humanScoreCounter.textContent = `Your Score: ${humanScore}`;
+    computerScoreCounter.textContent = `Computer's Score ${computerScore}`;
+    
+    playerMoveBox.replaceChildren();
+    compMoveBox.replaceChildren();
+    results.querySelectorAll("h1").forEach(h1 => h1.remove());
+    
+})
+
+
+
+
