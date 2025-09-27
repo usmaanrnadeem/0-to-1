20e917e8bfc14007dab53d93981537cd0b744408
usmaanrnadeem <usmaanrnadeem@outlook.com>
2025-09-27 16:57:02 +0100

    Complete 16_findTheOldest exercise

--- the-odin-project-foundations/js-foundations/js-exercises/16_findTheOldest/findTheOldest.js
+++ the-odin-project-foundations/js-foundations/js-exercises/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    for (item of arr) {
+        if (item.yearOfDeath == undefined) {
+            item.yearOfDeath = 2025
+        }
+        item.life = item.yearOfDeath - item.yearOfBirth
+    }
+    
+    return arr.sort((a,b) => b.life - a.life)[0]
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
