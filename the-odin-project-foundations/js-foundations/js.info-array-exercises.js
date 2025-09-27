c0a66d9233baf6558dffb87877fbe7440df5b536
usmaanrnadeem <usmaanrnadeem@outlook.com>
2025-09-27 14:29:07 +0100

    Complete Map to Names exercise

--- the-odin-project-foundations/js-foundations/js.info-array-exercises.js
+++ the-odin-project-foundations/js-foundations/js.info-array-exercises.js
@@ -68,3 +68,13 @@ function unique(arr) {
 }
 
 
+// Map to names 
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [ john, pete, mary ];
+
+let names = users.map(obj => obj.name)
+console.log(names);
