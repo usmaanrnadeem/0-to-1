b4cf413b7c1f3ff916e5597e4a0757ee816d82fd
usmaanrnadeem <usmaanrnadeem@outlook.com>
2025-09-27 16:31:17 +0100

    Complete 14_fibonacci exercise

--- the-odin-project-foundations/js-foundations/js-exercises/14_fibonacci/fibonacci.js
+++ the-odin-project-foundations/js-foundations/js-exercises/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS"
+    } else if (+num == 0) {
+        return 0
+    } else {
+        let fib = [1,1];
+        for (let i = 2; i < +num; i++) {
+            fib.push(fib[i-2]+fib[i-1])
+        }
+        return  fib.at(-1)
+    }
 };
 
 // Do not edit below this line
