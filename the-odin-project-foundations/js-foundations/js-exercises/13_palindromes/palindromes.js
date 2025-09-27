9cc800324358fabdb8f40dcc43f9073f4eb88b03
usmaanrnadeem <usmaanrnadeem@outlook.com>
2025-09-27 16:24:12 +0100

    Complete 13_palindromes exercise

--- the-odin-project-foundations/js-foundations/js-exercises/13_palindromes/palindromes.js
+++ the-odin-project-foundations/js-foundations/js-exercises/13_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    
+    let stringAsArray = str.toLowerCase().split("").filter(char => /[a-zA-Z0-9]/.test(char));
+    let reverseArray = (str.toLowerCase().split("").filter(char => /[a-zA-Z0-9]/.test(char))).reverse();
+
+
+
+    console.log(stringAsArray);
+    console.log(reverseArray)
+
+    if (JSON.stringify(stringAsArray) == JSON.stringify(reverseArray)) {
+        return true
+    } else {
+        return false
+    }
 };
+
+palindromes('Animal loots foliated detail of stool lamina.')
 
 // Do not edit below this line
 module.exports = palindromes;
