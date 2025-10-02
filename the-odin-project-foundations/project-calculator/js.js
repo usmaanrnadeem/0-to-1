74f8369529e17f9c945ef656abf1f3f3ae64a766
usmaanrnadeem <usmaanrnadeem@outlook.com>
2025-10-02 23:53:22 +0100

    Complete computational logic for the calculator and add basic styling

--- the-odin-project-foundations/project-calculator/js.js
+++ the-odin-project-foundations/project-calculator/js.js
@@ -16,77 +16,119 @@ function divide(a,b) {
 
 function operate(numberOne,numberTwo,operator) {
 
-    let answer;
-
     if (operator == "+") {
-        answer = add(numberOne,numberTwo)
+        answer = Math.round(add(numberOne,numberTwo)*100)/100
     } else if (operator == "-") {
-        answer = subtract(numberOne,numberTwo)
+        answer = Math.round(subtract(numberOne,numberTwo)*100)/100
     } else if (operator == '*') {
-        answer = multiply(numberOne,numberTwo) 
+        answer = Math.round(multiply(numberOne,numberTwo)*100)/100
     } else if (operator == '/') {
-        answer = divide(numberOne,numberTwo)
+        if (numberTwo !== "0") {
+            answer = Math.round(divide(numberOne,numberTwo)*100)/100
+        }
+        else {
+            alert("You cannot divide by 0, retry your calcualtion!");
+            clear();
+        }
+        
     }
 
     display.textContent = answer;
 
-    numberOne = answer;
+    return [answer, null, null];
+}
 
-    numberTwo = null;
-
-    operator = null;
+function clear() {
+    numberOne = 0;
+    numberTwo = 0;
+    operator = 0;
+    answer = 0;
+    firstDigitAfterEquals = null;
+    firstDigitAfterOperation = null;
+    display.textContent = "";
 }
 
 let numberOne;
 let operator;
 let numberTwo;
-let displayInput;
-let currentOperation;
 let answer;
-let firstDigitAfterOperation = false;
-let clickEvent = new Event('click');
+let firstDigitAfterOperation;
+let firstDigitAfterEquals;
+// let displayInput;
 
 const digits = document.querySelectorAll(".num");
 const display = document.querySelector(".display");
 const operations = document.querySelectorAll(".operation");
 const equals = document.querySelector(".equals");
+const clearBtn = document.querySelector(".clear");
 
+// For testing
 const one = document.querySelector(".numberOne")
 const two = document.querySelector(".numberTwo")
 const container = document.querySelector(".container")
-// 12 + 5 + 9 = 14
+// 12 + 7 - 1 = 18
 
 
 digits.forEach(btn => {
     btn.addEventListener("click", e => {
         if (firstDigitAfterOperation == true) {
             display.textContent = "";
-            firstDigitAfterOperation = false
+            firstDigitAfterOperation = false;
+            numberTwo = "fill me"
+        }
+
+        if (firstDigitAfterEquals == true) {
+            display.textContent = "";
+            numberOne = null;
+            numberTwo = null;
+            operator = null;
+            firstDigitAfterEquals = false;
         }
         display.textContent += e.target.textContent
-        displayInput = display.textContent
     })
 })
 
 operations.forEach(btn => {
-
     btn.addEventListener("click", e => {
-        if (currentOperation) {
-            computePending();
+        if (!numberOne) {
+            numberOne = display.textContent
         }
-        numberOne = displayInput;
-        currentOperation = e.target.textContent;
+        else if (numberOne !== display.textContent) {
+            numberTwo = display.textContent
+        }
+        
+        if (!operator) {
+            operator = e.target.textContent;
+        }
+        
+        if (numberOne && numberTwo && operator) {
+            [numberOne, numberTwo, operator] = operate(numberOne,numberTwo,operator);
+            operator = e.target.textContent;
+        }
         firstDigitAfterOperation = true; 
     })
 })
 
 equals.addEventListener("click", () => {
-    if (currentOperation) {
-        computePending()
+    if (numberOne && numberTwo && operator) {
+        if (numberTwo == "fill me") {
+            numberTwo = display.textContent;
+        }
+        [numberOne, numberTwo, operator] = operate(numberOne,numberTwo,operator);
     }
+    else if (numberOne && operator && numberTwo == "fill me") {
+        if (numberOne !== display.textContent) {
+            numberTwo = display.textContent;
+            [numberOne, numberTwo, operator] = operate(numberOne,numberTwo,operator);
+        }        
+    } 
+
+    numberOne = display.textContent;
+    numberTwo = null;
+
+    firstDigitAfterEquals = true;
 })
 
-container.addEventListener("click", () => {
-    one.textContent = numberOne;
-    two.textContent = numberTwo;
-})
+clearBtn.addEventListener("click", () => clear())
+
+//  Test
